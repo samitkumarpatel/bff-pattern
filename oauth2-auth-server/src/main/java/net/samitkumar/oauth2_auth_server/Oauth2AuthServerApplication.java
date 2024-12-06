2bb1d49f2925b80b89d81463b4a5e9cb20ca09e6
samitkumarpatel <samitkumarpatel@gmail.com>
2024-12-06 12:56:11 +0000

    bff poc

---
+++ oauth2-auth-server/src/main/java/net/samitkumar/oauth2_auth_server/Oauth2AuthServerApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.oauth2_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AuthServerApplication.class, args);
+	}
+
+}
---
+++ oauth2-auth-server/src/test/java/net/samitkumar/oauth2_auth_server/Oauth2AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.oauth2_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
