f458e746a07f48b7beb32f3df6cd73483db84f51
samitkumarpatel <samitkumarpatel@gmail.com>
2024-12-29 22:50:06 +0100

    one more resources added

--- json-placehosder-client/src/main/java/net/samitkumar/json_placehosder_client/JsonPlacehosderClientApplication.java
+++ json-placehosder-client/src/main/java/net/samitkumar/json_placehosder_client/JsonPlacehosderClientApplication.java
@@ -1,13 +1,13 @@
-package net.samitkumar.api_gateway;
+package net.samitkumar.json_placehosder_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class JsonPlacehosderClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(JsonPlacehosderClientApplication.class, args);
 	}
 
 }
---
+++ json-placehosder-client/src/test/java/net/samitkumar/json_placehosder_client/JsonPlacehosderClientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.json_placehosder_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlacehosderClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
